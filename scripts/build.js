5b715f60c020868e60d2565faeaf7f52ef3dd705
Derek Bredensteiner <derekbreden@gmail.com>
2025-07-10 03:00:05 -0500

    prevent ios zoom maybe

--- scripts/build.js
+++ scripts/build.js
@@ -268,7 +268,7 @@ async function extractText() {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, maximum-scale=1.0, initial-scale=1.0, user-scalable=no">
     <title>Mere Metaphor</title>
     <style>
         body {
